45d5693326e7cfb7833e1c1880812a2c5cb8340c
Himanshu Goswami <himanshugoswami.12feb@gmail.com>
2025-08-10 01:17:02 +0530

    Return to last edit position when opening file again

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -18,3 +18,14 @@ vim.api.nvim_create_autocmd("BufEnter", {
     desc = "Disable New Line Comment",
 })
 
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = augroup,
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')   -- " is the last cursor position mark in vim/neovim
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
