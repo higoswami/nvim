b6055241785f436135c2225692c95bfd5ea1fc0d
Himanshu Goswami <himanshugoswami.12feb@gmail.com>
2025-08-11 13:47:00 +0530

    Change indentation based on filetype

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,11 +1,11 @@
-local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+local user_group = vim.api.nvim_create_augroup("UserConfig", {})
 
 -- Highlight on yank Autocmd --
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank()
     end,
-    group = augroup,
+    group = user_group,
 })
 
 -- This disables automatic comments in all filetypes
@@ -14,13 +14,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
     callback = function()
         vim.opt.formatoptions:remove({ "c", "r", "o" })
     end,
-    group = augroup,
+    group = user_group,
     desc = "Disable New Line Comment",
 })
 
 -- Return to last edit position when opening files
 vim.api.nvim_create_autocmd("BufReadPost", {
-    group = augroup,
     callback = function()
         local mark = vim.api.nvim_buf_get_mark(0, '"')   -- " is the last cursor position mark in vim/neovim
         local lcount = vim.api.nvim_buf_line_count(0)
@@ -28,4 +27,16 @@ vim.api.nvim_create_autocmd("BufReadPost", {
             pcall(vim.api.nvim_win_set_cursor, 0, mark)
         end
     end,
+    group = user_group,
 })
+
+-- Change Indentation based on fileType
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "javascript", "typescript", "json", "html", "css" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
+    group = user_group,
+})
+
