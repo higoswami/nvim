461a7d45f606df73b2f6e95a2f297edb0d8112bc
Himanshu Goswami <himanshugoswami.12feb@gmail.com>
2025-08-20 03:29:04 +0530

    Updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 These config requires atleast NVIM v0.12 becuase i'm using vim.pack (Built-in Package Manager)
 
+## Update the Plugins
+
+```vim
+:lua vim.pack.update()
+```
+
+Execute `:write` to confirm update, execute `:quit` to discard the update.
+
+```vim
+:write
+```
+
+
 ## References
 - [Minimal Neovim Config](https://github.com/radleylewis/nvim-lite)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,7 +7,8 @@ vim.pack.add({
     {src = "https://github.com/echasnovski/mini.files.git"},
     {src = "https://github.com/echasnovski/mini.pick.git"},
     {src = "https://github.com/lewis6991/gitsigns.nvim.git"},
-    {src = 'https://github.com/dhananjaylatkar/cscope_maps.nvim.git', version = 'main'},
+    {src = "https://github.com/dhananjaylatkar/cscope_maps.nvim.git", version = "main"},
+
     {src = "https://github.com/nvim-treesitter/nvim-treesitter.git", version = "master"}, -- All future updates will be on main branch
     {src = "https://github.com/nvim-treesitter/nvim-treesitter-context.git"},
 })
